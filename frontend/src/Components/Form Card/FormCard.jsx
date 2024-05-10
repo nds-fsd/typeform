d5236dd85a3cdafed31bf0efb0cfa2385c6223cf
Jose Luis Halcartegaray <joseluis.hver@gmail.com>
2024-05-10 20:36:01 +0200

    "Conectado Workspace con backend, useEffect Get "

--- frontend/src/Components/Form Card/FormCard.jsx
+++ frontend/src/Components/Form Card/FormCard.jsx
@@ -6,37 +6,37 @@ const FormCard = () => {
 
     const [forms, setForms] = useState([])
 
-    // useEffect(() => {
-    //     const url = 'http://localhost:3001/forms'; //reemplazar url real
-    //     const getForms = async () => {
-    //         try {
-    //             const response = await fetch (url);
-    //             if (!response.ok) throw new Error('Something bad happended');
-    //             const json = await response.json();
-    //             setForms(json);
-    //         } catch(error) {
-    //             alert(error);
-    //         }
-    //     }
-    //     getForms();
-    // }, []);
-
     useEffect(() => {
-        const getForms = [
-        {
-            title: "form 1"
-        },
-        {
-            title: "form 2"
-        },
-        {
-            title: "form 3"
+        const url = 'http://localhost:3001/form'; //reemplazar url real
+        const getForms = async () => {
+            try {
+                const response = await fetch (url);
+                if (!response.ok) throw new Error('Something bad happended');
+                const json = await response.json();
+                setForms(json);
+            } catch(error) {
+                alert(error);
+            }
         }
-    ]; //reemplazar esto por el fetch
+        getForms();
+    }, []);
+
+    // useEffect(() => {
+    //     const getForms = [
+    //     {
+    //         title: "form 1"
+    //     },
+    //     {
+    //         title: "form 2"
+    //     },
+    //     {
+    //         title: "form 3"
+    //     }
+    // ]; //reemplazar esto por el fetch
 
 
-    setForms(getForms); 
-}, []);
+//     setForms(getForms); 
+// }, []);
     
 
     return (
