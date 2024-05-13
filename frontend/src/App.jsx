ec3302f03a57880558a6d41de4083e7d8a7f8990
AndressaSponton <andressatricolor@gmail.com>
2024-05-13 19:35:42 +0200

    added footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import NewForm from './components/NewForm';
-import styles from './components/App.module.css';
+import CreateForms from './components/CreateForms';
 
 const App = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <div style={{ marginBottom: '50px' }}>
-        <NewForm />
-      </div>
-      <footer
-        className={styles.footerNewForm}
-        style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: '#f0f0f0', padding: '10px' }}
-      >
-        <button
-          className={styles.saveButton}
-          type='submit'
-          style={{ position: 'fixed', bottom: '10px', right: '10px' }}
-        >
-          Save
-        </button>
-      </footer>
+    <div>
+      <CreateForms />
     </div>
   );
 };
