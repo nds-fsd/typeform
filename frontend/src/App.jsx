946559e0a3c12ec334c311cb9fae815397d9989a
Jose Luis Halcartegaray <joseluis.hver@gmail.com>
2024-05-14 11:12:36 +0200

    ""

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import {Routes, Route} from "react-router-dom";
 import Home from "./Components/Home/Home.jsx";
 import Workspace from "./Components/Workspace/Workspace.jsx";
-import CreateForms from "./components/CreateForms.jsx";
+import CreateForms from "./Components/CreateForms.jsx";
 
 
 
