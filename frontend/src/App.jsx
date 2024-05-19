0573767a32afa40172e53a09e685cc8b7f31066d
JoelGR05 <joel.gonzalezrod@gmail.com>
2024-05-19 18:10:59 +0200

    removed signup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home.jsx';
 import Workspace from './Components/Workspace/Workspace.jsx';
 import CreateForms from './Components/CreateForms/CreateForms.jsx';
-import SignUp from './Components/SignUp/SignUp.jsx';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Route path='/home' element={<Home />} />
       <Route path='/workspace' element={<Workspace />} />
       <Route path='/createforms' element={<CreateForms />} />
-      <Route path='/signup' element={<SignUp />} />
     </Routes>
   );
 }
