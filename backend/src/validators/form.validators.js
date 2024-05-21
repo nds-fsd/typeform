4da9a5d7b196869a7dbfa2029701c9e87ec7f39c
haudacia <hadassa.codes@gmail.com>
2024-05-21 13:41:38 +0200

    revert comment code (previous debug)

--- backend/src/validators/form.validators.js
+++ backend/src/validators/form.validators.js
@@ -1,21 +1,20 @@
 const z = require('zod');
 const { QuestionSchema } = require('../schemas/question.schema');
 
-console.log(QuestionSchema);
-// const questionValidationSchema = z.object({
-//   text: z.string(),
-//   description: z.string().optional(),
-//   type: z.enum(Object.keys(QuestionSchema.discriminators)),
-//   choices: z.array(z.object({
-//     label: z.string(),
-//   })).optional(),
-// }).refine((data) => {
-//   if (data.type === 'MultipleChoiceQuestion' || data.type === 'SingleChoiceQuestion') {
-//     return data.choices.length > 1;
-//   }
-// });
+const questionValidationSchema = z.object({
+  text: z.string(),
+  description: z.string().optional(),
+  type: z.enum(Object.keys(QuestionSchema.discriminators)),
+  choices: z.array(z.object({
+    label: z.string(),
+  })).optional(),
+}).refine((data) => {
+  if (data.type === 'MultipleChoiceQuestion' || data.type === 'SingleChoiceQuestion') {
+    return data.choices.length > 1;
+  }
+});
 
-// exports.CreateFormBodyValidation = z.object({
-//   title: z.string(),
-//   questions: z.array(questionValidationSchema).optional(),
-// })
+exports.CreateFormBodyValidation = z.object({
+  title: z.string(),
+  questions: z.array(questionValidationSchema).optional(),
+})
