fc097d259bd2da9fb445c4933e2f01338efca630
AndressaSponton <andressatricolor@gmail.com>
2024-05-22 10:27:18 +0200

    saving changes before changing branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect } from "react";
-import {Routes, Route} from "react-router-dom";
-import Home from "./Components/Home/Home.jsx";
-import Workspace from "./Components/Workspace/Workspace.jsx";
-import CreateForms from "./Components/CreateForms/CreateForms.jsx";
-import Auth from "./Components/Auth/Auth.jsx";
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home/Home.jsx';
+import Workspace from './Components/Workspace/Workspace.jsx';
+import CreateForms from './Components/CreateForms/CreateForms.jsx';
+import Auth from './Components/Auth/Auth.jsx';
 
 function App() {
-
-    return (
-            <Routes> 
-                <Route path='/home' element={<Home />} />
-                <Route path='/workspace' element={<Workspace />} />  
-                <Route path='/createforms' element={<CreateForms />} />
-                <Route path="/auth" element={<Auth />} />            </Routes>  
-    ) 
-
+  return (
+    <Routes>
+      <Route path='/home' element={<Home />} />
+      <Route path='/workspace' element={<Workspace />} />
+      <Route path='/createforms' element={<CreateForms />} />
+      <Route path='/auth' element={<Auth />} />
+    </Routes>
+  );
 }
 
 export default App;
--- frontend/src/Components/Workspace/Workspace.jsx
+++ frontend/src/Components/Workspace/Workspace.jsx
@@ -3,20 +3,21 @@ import FormCard from '../Form Card/FormCard';
 import style from './Workspace.module.css';
 
 const handleClick = () => {
-    window.location.href = 'http://localhost:3000/createforms'
-} 
+  window.location.href = 'http://localhost:3000/createforms';
+};
 
 const Workspace = () => {
-    return (
-        <div className={style.viewport}>
-            <h1>My Workspace</h1>
-            <div className={style.frame}>
-                <button className={style.btn} onClick={handleClick}>Add New Form</button>
-                <FormCard className={style.formcard} /> 
-            </div>
-        </div>
-        
-    )
-}
+  return (
+    <div className={style.viewport}>
+      <h1>My Workspace</h1>
+      <div className={style.frame}>
+        <button className={style.btn} onClick={handleClick}>
+          Add New Form
+        </button>
+        <FormCard className={style.formcard} />
+      </div>
+    </div>
+  );
+};
 
 export default Workspace;
