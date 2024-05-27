e3d0eed783968069b3355c23ed13678de1b04d77
Jose Luis Halcartegaray <joseluis.hver@gmail.com>
2024-05-27 09:51:39 +0200

    "Cambiado err por error y quitado route login"

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -25,7 +25,7 @@ const login = async (req, res) => {
          },
       })
    })
-.catch((err) => {
+.catch((error) => {
    return res.status(500).json( { error: { register: "Error login in:", error }})
 })
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
       <Route path='/home' element={<Home />} />
       <Route path='/auth' element={<Auth />} />
       <Route path='/signup' element={<SignUp />} />
-      <Route path='/login'element={<Login />} />
       {token && (
         <>
           <Route path='/workspace' element={<Workspace />} />
