c28f4e6495575efcb20f02cfa77da8857d737885
JoelGR05 <joel.gonzalezrod@gmail.com>
2024-05-29 19:27:34 +0200

    paowikjdpoa

--- backend/src/middlewares/Authorization.js
+++ backend/src/middlewares/Authorization.js
@@ -1,19 +1,19 @@
-const { jwt } = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const User = require('../schemas/user.schema');
 
-const userAuthorization = async (req, res, next) => {
-  const authorization = req.headers['authorization'];
+const Authorization = async (req, res, next) => {
+  const { authorization } = req.headers;
 
   if (!authorization) {
     res.status(401).json({ error: 'Not Authorized, no token' });
   }
-  const token = header.split(' ')[1];
+  const token = authorization.split(' ')[1];
 
   try {
     const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
-    next();
     const user = await User.findOne({ email: verifyToken.email });
     req.user = user;
+    next();
   } catch (error) {
     console.log(error);
     res.status(401).json({ error: 'Not authorized' });
@@ -21,5 +21,5 @@ const userAuthorization = async (req, res, next) => {
 };
 
 module.exports = {
-  userAuthorization,
+  Authorization,
 };
--- backend/src/routers/routes/form.js
+++ backend/src/routers/routes/form.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getAllForms, getForm, createForm, deleteForm, updateForm } = require('../../controllers/form.controller');
+const { Authorization } = require('../../middlewares/Authorization');
 
 const router = express.Router();
 
-router.get('/', getAllForms);
-router.get('/:id', getForm);
+router.get('/', Authorization, getAllForms);
+router.get('/:id', Authorization, getForm);
 router.post('/', createForm);
-router.patch('/:id', updateForm);
-router.delete('/:id', deleteForm);
+router.patch('/:id', Authorization, updateForm);
+router.delete('/:id', Authorization, deleteForm);
 
 module.exports = router;
