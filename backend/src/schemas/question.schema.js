b9b4c365c2f70986b7ffd986d3b22e295e32ea28
haudacia <hadassa.codes@gmail.com>
2024-05-30 11:27:03 +0200

    remove comment

--- backend/src/schemas/question.schema.js
+++ backend/src/schemas/question.schema.js
@@ -46,46 +46,4 @@ console.log(Object.keys(QuestionSchema._applyDiscriminators)); // This should pr
 
 const Question = model('Question', QuestionSchema)
 
-module.exports = {
-    QuestionSchema,
-    Question,
-    TextQuestion,
-    MultipleChoiceQuestion,
-    SingleChoiceQuestion,
-    YesNoQuestion
-};
-
-// // OTRA ALTERNATIVA, MÁS "SIMPLES"(DISCRIMINATORS DECLARADOS DENTRO DEL PROPIO SCHEMA)
-// const { Schema, model } = require('mongoose');
-
-// const QuestionSchema = new Schema({
-//     text: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String,
-//         required: false
-//     },
-//     creationDateTime: {
-//         type: Date,
-//         default: Date.now,
-//         immutable: true
-//     },
-//     updateDateTime: {
-//         type: Date,
-//         default: Date.now
-//     },
-//     type: {
-//         type: String,
-//         enum: ['TextQuestion', 'MultipleChoiceQuestion', 'SingleChoiceQuestion', 'YesNoQuestion'],
-//         required: true
-//     },
-//     choices: [{
-//         label: String
-//     }]
-// });
-
-// const Question = model('Question', QuestionSchema);
-
-// module.exports = { Question };
+module.exports = { Question };
