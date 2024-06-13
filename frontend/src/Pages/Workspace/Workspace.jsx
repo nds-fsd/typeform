d2c9ed1091639fb8f76b39dcff6a9d16c461998b
haudacia <hadassa.codes@gmail.com>
2024-06-13 19:54:50 +0200

    upon click on Add new form (/workspace), substitute /cre\ateforms by /createform

--- frontend/src/Pages/Workspace/Workspace.jsx
+++ frontend/src/Pages/Workspace/Workspace.jsx
@@ -4,20 +4,6 @@ import FormCard from '../Form Card/FormCard';
 import style from './Workspace.module.css';
 import { api } from '../../utils/api';
 
-const handleClick = () => {
-  // const createForm = async () => {
-  //   try {
-  //     const res = await api().post(`/form`)
-  //     console.log(res.data)
-  //     return res.data;
-  //   } catch (error) {
-  //     console.log('Error fetching form')
-  //   };
-  // };
-  // createForm();
-
-
-};
 
 const Workspace = () => {
   const navigate = useNavigate();
@@ -26,7 +12,7 @@ const Workspace = () => {
     <div className={style.viewport}>
       <h1>My Workspace</h1>
       <div className={style.frame}>
-        <button className={style.btn} onClick={() => navigate('/createforms')}>
+        <button className={style.btn} onClick={() => navigate('/createform')}>
           Add New Form
         </button>
         <FormCard className={style.formcard} />
@@ -34,21 +20,3 @@ const Workspace = () => {
     </div >
   );
 };
-
-export default Workspace;
-
-//// tentativa de crear un nuevo form al hacer click en el boton
-////y luego despues dirigirse a /createform/:newlyCreated_id
-// const Workspace = () => {
-//   const history = useHistory();
-
-//   const handleClick = async () => {
-//     try {
-//       const res = await api().post(`/form`);
-//       console.log(res.data);
-//       // va a la pagina solo despues de haber creado un nuevo form
-//       history.push('/createforms');
-//     } catch (error) {
-//       console.log('Error creating form:', error);
-//     }
-//   };
