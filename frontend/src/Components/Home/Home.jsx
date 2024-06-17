8f95256bb20bfca2f5b97e638511eb5992feec4b
joseluishver <joseluis.hver@gmail.com>
2024-06-17 13:16:20 +0200

    Home ready with styling

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import style from './Home.module.css';
 
 const Home = () => {
   return (
-    <div className="bg-custom-gradient">    
-      <div className={style.container}>
-        <div className={style.welcome}>
-          <div className={style.logo}>
-            <img style={{width: 318, height: 60 }} src='src/Assets/Surflow logo@2x.png' alt="Surflow Logo" />
+    <div className="bg-custom-gradient min-h-screen flex items-center justify-center p-2">
+      <div className="bg-white p-10 rounded-3xl shadow-md flex flex-col items-center w-full h-[90vh] mx-4">
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="mb-36">
+            <img style={{ width: 318, height: 60 }} src='src/Assets/Surflow logo@2x.png' alt="Surflow Logo" />
           </div>
-          <div className='flex flex-col'>  
-            <h1> Are your surveys ready to <span className={style.flow}>FLOW</span>? </h1>
-            <Link to="/login">
-              <button className="w-80 text-gray-900 h-20 shadow-md bg-azure hover:bg-white 
-          hover:shadow-none hover:border hover:border-gray-600 rounded-4xl transition-all
-          duration-300 font-space mono scale-150"><span className="font-bold">Yes!</span>, Let's do this!</button>
-            </Link>
-          </div>
+          <h1 className=" text-gray-900 text-center text-3xl mb-24">Are your surveys ready to <span className="text-gray-900 font-bold underline">FLOW</span>?</h1>
+          <Link to="/login">
+            <button className="w-80 text-gray-900 h-20 shadow-md bg-azure hover:bg-white 
+              hover:shadow-none hover:border hover:border-gray-600 rounded-4xl transition-all
+              duration-300 font-space mono scale-150">
+              <span className="font-bold">Yes!</span>, Let's do this!
+            </button>
+          </Link>
         </div>
       </div>    
     </div>
