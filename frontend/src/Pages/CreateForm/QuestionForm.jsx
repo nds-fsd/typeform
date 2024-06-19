155cd4f98cfcff39beb2457bdb01d08b8a42b207
haudacia <hadassa.codes@gmail.com>
2024-06-19 03:13:10 +0200

    WIP QuestionChoices context

--- frontend/src/Pages/CreateForm/QuestionForm.jsx
+++ frontend/src/Pages/CreateForm/QuestionForm.jsx
@@ -1,3 +1,87 @@
+// import React, { useEffect, useRef, useContext } from 'react';
+// import styles from './FormForm.module.css';
+// import { useOutletContext, useParams } from 'react-router-dom';
+// import Footer from './Footer';
+// import QuestionChoices from './QuestionChoices';
+// import { api } from '../../utils/api';
+// import { FormContext, useFormProvider } from '../../context/FormContext';
+
+// const questionTypes = [
+//     { value: 'TextQuestion', label: 'Text' },
+//     { value: 'MultipleChoiceQuestion', label: 'Multiple Choice' },
+//     { value: 'SingleChoiceQuestion', label: 'Single Choice' },
+//     { value: 'YesNoQuestion', label: 'Yes/No' }
+// ];
+
+// const QuestionForm = ({ onSubmit }) => {
+//     const {
+//         selectedQuestion,
+//         setSelectedQuestion,
+//         allForms,
+//         setAllForms,
+//         fields,
+//         register,
+//         watch,
+//         control,
+//         handleSubmit
+//     } = useFormProvider();
+
+//     const index = selectedQuestion && fields.indexOf(selectedQuestion)
+//     const type = selectedQuestion && selectedQuestion.type;
+//     console.log(type, index, 'tipo e index');
+//     console.log('all forms: ', allForms)
+
+//     // if (!selectedQuestion) return <div>Loading...</div>;
+
+//     return (
+//         <div>
+//             <form
+//                 onSubmit={handleSubmit(onSubmit)}
+//                 onBlur={handleSubmit(onSubmit)}
+//                 className={styles.form}
+//             >
+//                 <div>
+//                     <select {...register(`allForms[${index}].type`)}>
+//                         {questionTypes.map((questionType, index) => (
+//                             <option value={questionType.value} key={questionType.id}>
+//                                 {questionType.label}
+//                             </option>
+//                         ))}
+//                     </select>
+//                     <input
+//                         id={styles.inputQuestionText}
+//                         type="text"
+//                         placeholder='write your question here'
+//                         {...register(`questions[${index}].text`)}
+//                     />
+//                     <p>{selectedQuestion.text}</p>
+//                     <h1>{selectedQuestion.type}</h1>
+//                     {/* <pre>{JSON.stringify(allForms, null, 2)}</pre> */}
+
+
+//                     <input
+//                         id={styles.inputQuestionDescription}
+//                         type="text"
+//                         placeholder='optional description'
+//                         {...register(`questions[${index}].description`)}
+//                     />
+//                     {type !== 'TextQuestion' && (
+//                         <QuestionChoices
+//                             register={register}
+//                             control={control}
+//                             index={index}
+//                             isYesNo={type === 'YesNoQuestion'}
+//                         />
+//                     )}
+
+//                 </div>
+//             </form>
+//         </div>
+//     );
+// };
+
+// export default QuestionForm;
+
 import React, { useEffect, useRef, useContext } from 'react';
 import styles from './FormForm.module.css';
 import { useOutletContext, useParams } from 'react-router-dom';
@@ -23,15 +107,32 @@ const QuestionForm = ({ onSubmit }) => {
         register,
         watch,
         control,
-        handleSubmit
+        handleSubmit,
+        onEditForm
     } = useFormProvider();
 
-    const index = selectedQuestion && fields.indexOf(selectedQuestion)
+    const { idQuestion } = useParams();
+    console.log(idQuestion)
+
+    //index of question: clean up unnecessary code and make calling the _id as
+    // direct as possible
+    //console.log('recebido:', fields, typeof fields)
+    // const selectedQuestion = fields.find(question => question._id === idQuestion);
+    const index = fields.indexOf(selectedQuestion)
     const type = selectedQuestion && selectedQuestion.type;
     console.log(type, index, 'tipo e index');
     console.log('all forms: ', allForms)
 
     // if (!selectedQuestion) return <div>Loading...</div>;
+
+    useEffect(() => {
+        console.log('selectedQuestion:', selectedQuestion);
+        console.log('index:', index);
+        // console.log('type:', type);
+        console.log(idQuestion);
+    }, [selectedQuestion, type, idQuestion]);
+
+    if (!selectedQuestion) return <div>Loading...</div>;
 
     return (
         <div>
@@ -41,7 +142,7 @@ const QuestionForm = ({ onSubmit }) => {
                 className={styles.form}
             >
                 <div>
-                    <select {...register(`allForms[${index}].type`)}>
+                    <select {...register(`questions[${index}].type`)}>
                         {questionTypes.map((questionType, index) => (
                             <option value={questionType.value} key={questionType.id}>
                                 {questionType.label}
@@ -55,9 +156,6 @@ const QuestionForm = ({ onSubmit }) => {
                         {...register(`questions[${index}].text`)}
                     />
                     <p>{selectedQuestion.text}</p>
-                    <h1>{selectedQuestion.type}</h1>
-                    {/* <pre>{JSON.stringify(allForms, null, 2)}</pre> */}
-
 
                     <input
                         id={styles.inputQuestionDescription}
@@ -67,8 +165,6 @@ const QuestionForm = ({ onSubmit }) => {
                     />
                     {type !== 'TextQuestion' && (
                         <QuestionChoices
-                            register={register}
-                            control={control}
                             index={index}
                             isYesNo={type === 'YesNoQuestion'}
                         />
