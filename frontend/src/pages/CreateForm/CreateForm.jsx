61583a40baa9a407b95eb59edf229ff01bec480d
haudacia <hadassa.codes@gmail.com>
2024-06-27 14:22:11 +0200

    undo merge mistakes

--- frontend/src/pages/CreateForm/CreateForm.jsx
+++ frontend/src/pages/CreateForm/CreateForm.jsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import { api } from '../../utils/api.js';
-import { api, fetchForm } from '../../utils/api.js';
-import { useQuery, useQueryClient } from 'react-query';
 import { useNavigate, useParams } from 'react-router-dom';
 import { QuestionList } from './QuestionList.jsx';
 import QuestionForm from './QuestionForm.jsx';
@@ -17,43 +15,8 @@ export const CreateForm = withCustomFormProvider(() => {
   const { handleSubmit, setValue } = useCustomFormProvider();
   const currentForm = forms?.find((form) => form._id === id);
 
-  const isEditMode = !!id;
-  console.log('edit mode?', isEditMode)
-
-  // GET form by id (used in CreateForm and its children), previously:
-  // const { formData } = useQuery('form', () => api().get(`/form/${id}`).then(res => res.data));
-  // como insertar error, isLoading y isError aqui, para que quede consistente,
-  // sin que haya conflicto con los del useQUery anterior (allForms)?
-  const { data: formData } = useQuery({
-    queryKey: ['form'],
-    queryFn: () => api().get(`/form/${id}`).then(res => res.data)
-  });
-
-  console.log("formdata ok", formData);
-
-  // version funcional de TYP-30:   
-  // useEffect(() => {
-  //   if (data) {
-  //     console.log("data", data);
-  //     setValue('title', data.title || '');
-  //     setValue('questions', data.questions.map((question) => ({
-  //       ...question,
-  //       choices: question.choices || [],
-  //     })) || []);
-  //   }
-  // }, [data, setValue]);
-
-  // useEffect(() => {
-  //   if (isEditMode && currentForm) {
-  //     setValue('title', currentForm.title || '');
-  //     setValue('questions', currentForm.questions.map((question) => ({
-  //       ...question,
-  //       choices: question.choices || [],
-  //     })) || []);
-  //   } else {
-  //     resetForm();
-  //   }
-  // }, [setValue, currentForm]);
+  const isEditMode = !!id && currentForm;
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isEditMode) {
@@ -65,44 +28,35 @@ export const CreateForm = withCustomFormProvider(() => {
           choices: question.choices || [],
         })) || [],
       );
-      if (isEditMode && formData) {
-        setValue('title', formData.title || '');
-        setValue('questions', formData.questions.map((question) => ({
-          ...question,
-          choices: question.choices || [],
-        })) || []);
-      } else {
-        resetForm();
-      }
-    }, [isEditMode]);
-}, [setValue, formData]);
+    }
+  }, [isEditMode]);
 
-const onSubmit = (data) => {
-  if (isEditMode) {
-    api()
-      .patch(`/form/${id}`, data)
-      .then((response) => {
-        queryClient.invalidateQueries('forms').then(() => navigate('/workspace'));
-      });
-  } else {
-    api()
-      .post('/form', data)
-      .then((response) => {
-        queryClient.invalidateQueries('forms').then(() => navigate('/workspace'));
-      });
-  }
-};
-return (
-  <div className='bg-custom-gradient p-2 box-border h-screen'>
-    {!isLoading && (
-      <form className='h-full' onSubmit={handleSubmit(onSubmit)}>
-        <div className='flex h-full'>
-          <QuestionList />
-          <QuestionForm />
-          <QuestionOptions />
-        </div>
-      </form>
-    )}
-  </div>
-);
+  const onSubmit = (data) => {
+    if (isEditMode) {
+      api()
+        .patch(`/form/${id}`, data)
+        .then((response) => {
+          queryClient.invalidateQueries('forms').then(() => navigate('/workspace'));
+        });
+    } else {
+      api()
+        .post('/form', data)
+        .then((response) => {
+          queryClient.invalidateQueries('forms').then(() => navigate('/workspace'));
+        });
+    }
+  };
+  return (
+    <div className='bg-custom-gradient p-2 box-border h-screen'>
+      {!isLoading && (
+        <form className='h-full' onSubmit={handleSubmit(onSubmit)}>
+          <div className='flex h-full'>
+            <QuestionList />
+            <QuestionForm />
+            <QuestionOptions />
+          </div>
+        </form>
+      )}
+    </div>
+  );
 });
