e2ef2f5f2b297c916e073956cd228a53735f4251
haudacia <hadassa.codes@gmail.com>
2024-07-01 15:15:07 +0200

    create login test file, install cypress

---
+++ frontend/src/e2e/login.cy.js
@@ -0,0 +1,9 @@
+describe('Login flow', () => {
+    const email = 'h@gmail.com';
+    const password = 'password';
+
+
+    it('Can login', () => {
+        cy.visit('http://localhost:3000');
+    })
+})
