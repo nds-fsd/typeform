aa1849bdb7607497d8e0d9cf0ea855cdce6a164b
JoelGR05 <joel.gonzalezrod@gmail.com>
2024-07-04 19:03:35 +0200

    fixed multiplechoicequestion problem & css

--- frontend/src/Pages/ResponsePage/RenderQuestion.jsx
+++ frontend/src/Pages/ResponsePage/RenderQuestion.jsx
@@ -6,22 +6,21 @@ const RenderQuestion = ({ question, index, register, style }) => {
   switch (question.type) {
     case 'TextQuestion':
       return (
-        <div className='max-w-6xl flex flex-column' key={question._id}>
-          <label className='text-4xl'>{question.text}</label>
-          <p className='text-2xl flex justify-center flex-row max-w-6xl'>{question.description}</p>
-          <input
-            className='w-full p-2 border border-gray-300 rounded'
-            type='text'
+        <div className='max-w-6xl flex flex-col' key={question._id}>
+          <label className='text-4xl font-semibold mb-2'>{question.text}</label>
+          <p className='text-2xl max-w-6xl font-medium'>{question.description}</p>
+          <textarea
+            className='w-full p-2 border border-gray-300 rounded my-8 h-auto resize-none z-50 '
             {...register(`questions.${index}.answer`, { required: true })}
           />
         </div>
       );
     case 'SingleChoiceQuestion':
       return (
-        <div className='w-full' key={question._id}>
-          <label className='text-4xl font-semibold mb-2'>{question.text}</label>
-          <p className='text-lg mb-4'>{question.description}</p>
-          <div className='flex flex-row flex wrap content-between'>
+        <div className='max-w-6xl flex flex-col' key={question._id}>
+          <label className='font text-4xl font-semibold mb-2'>{question.text}</label>
+          <p className='text-2xl max-w-6xl font-medium'>{question.description}</p>
+          <div className='flex flex-row flex wrap content-between justify-around mt-10 z-50'>
             {question.choices.map((option) => (
               <div key={option._id}>
                 <input
@@ -39,10 +38,10 @@ const RenderQuestion = ({ question, index, register, style }) => {
       );
     case 'MultipleChoiceQuestion':
       return (
-        <div className='max-w-6xl flex flex-column' key={question._id}>
-          <label className='text-4xl'>{question.text}</label>
-          <p className='text-2xl flex justify-center flex-row max-w-6xl'>{question.description}</p>
-          <div className='flex flex-row flex wrap content-between'>
+        <div className='max-w-6xl flex flex-col' key={question._id}>
+          <label className='text-4xl font-semibold mb-2'>{question.text}</label>
+          <p className='text-2xl max-w-6xl font-medium'>{question.description}</p>
+          <div className='flex flex-row flex wrap content-between justify-around mt-10 z-50'>
             {question.choices.map((option) => (
               <div key={option._id}>
                 <input
@@ -61,36 +60,28 @@ const RenderQuestion = ({ question, index, register, style }) => {
     case 'YesNoQuestion':
       return (
         <div className='max-w-6xl flex flex-col' key={question._id}>
-          <label className='text-4xl'>{question.text}</label>
-          <p className='text-2xl flex justify-center flex-row max-w-6xl'>{question.description}</p>
-          <div className='flex gap-2 mt-4'>
-            <div
-              className='border border-blue-300 bg-blue-100 px-4 py-2 rounded-lg flex items-center cursor-pointer hover:bg-blue-200'
-              onClick={() => document.getElementById(`${question._id}_yes`).click()}
-            >
-              <span className='border border-blue-300 w-6 h-6 flex items-center justify-center mr-2'>A</span>
+          <label className='text-4xl font-semibold mb-2'>{question.text}</label>
+          <p className='text-2xl max-w-6xl font-medium'>{question.description}</p>
+          <div className='flex flex-row flex-wrap content-between justify-around mt-10 z-50'>
+            <div>
               <input
                 type='radio'
-                id={`${question._id}_yes`}
-                className='opacity-0 absolute'
+                id={`${question._id}-yes`}
+                className='text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9 focus:bg-indigo-600 focus:text-white hover:bg-neutral-100'
                 value='Yes'
                 {...register(`questions.${index}.answer`, { required: true })}
               />
-              <label className='ml-2 cursor-pointer'>Yes</label>
+              <label htmlFor={`${question._id}-yes`}>Yes</label>
             </div>
-            <div
-              className='border border-blue-300 bg-blue-100 px-4 py-2 rounded-lg flex items-center cursor-pointer hover:bg-blue-200'
-              onClick={() => document.getElementById(`${question._id}_no`).click()}
-            >
-              <span className='border border-blue-300 w-6 h-6 flex items-center justify-center mr-2'>B</span>
+            <div>
               <input
                 type='radio'
-                id={`${question._id}_no`}
-                className='opacity-0 absolute'
+                id={`${question._id}-no`}
+                className='text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9 focus:bg-indigo-600 focus:text-white hover:bg-neutral-100'
                 value='No'
                 {...register(`questions.${index}.answer`, { required: true })}
               />
-              <label className='ml-2 cursor-pointer'>No</label>
+              <label htmlFor={`${question._id}-no`}>No</label>
             </div>
           </div>
         </div>
@@ -101,3 +92,38 @@ const RenderQuestion = ({ question, index, register, style }) => {
 };
 
 export default RenderQuestion;
+
+// case 'YesNoQuestion':
+//       return (
+//         <div className='max-w-6xl flex flex-col' key={question._id}>
+//           <label className='text-4xl font-semibold mb-2'>{question.text}</label>
+//           <p className='text-2xl max-w-6xl font-medium'>{question.description}</p>
+//           <div className='flex flex-row flex wrap content-between justify-center my-16'>
+//             <div className='mx-16 border border-blue-300 bg-blue-100 px-4 py-2 rounded-lg flex items-center cursor-pointer hover:bg-blue-200 z-50'>
+//               <span className='border border-blue-300 w-6 h-6 flex items-center justify-center mr-2'>A</span>
+//               <input
+//                 type='radio'
+//                 id={`${question._id}_yes`}
+//                 className='opacity-0 absolute'
+//                 value='Yes'
+//                 {...register(`questions.${index}.answer`, { required: true })}
+//               />
+//               <label className='ml-2 cursor-pointer'>Yes</label>
+//             </div>
+//             <div className='mx-16 border border-blue-300 bg-blue-100 px-4 py-2 rounded-lg flex items-center cursor-pointer hover:bg-blue-200'>
+//               <span className='border border-blue-300 w-6 h-6 flex items-center justify-center mr-2'>B</span>
+//               <input
+//                 type='radio'
+//                 id={`${question._id}_no`}
+//                 className='opacity-0 absolute'
+//                 value='No'
+//                 {...register(`questions.${index}.answer`, { required: true })}
+//               />
+//               <label className='ml-2 cursor-pointer'>No</label>
+//             </div>
+//           </div>
+//         </div>
+//       );
+//     default:
+//       return null;
+//   }
