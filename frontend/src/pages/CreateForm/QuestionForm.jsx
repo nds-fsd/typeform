0fa916440838216ab867d74cd94d4e92c5ecc0d9
haudacia <hadassa.codes@gmail.com>
2024-07-13 19:28:05 +0200

    rename to onBlurAutosave

--- frontend/src/pages/CreateForm/QuestionForm.jsx
+++ frontend/src/pages/CreateForm/QuestionForm.jsx
@@ -5,7 +5,7 @@ import { useFormState } from 'react-hook-form';
 import TextareaAutoSize from 'react-textarea-autosize';
 import YesNoChoices from './YesNoChoices.jsx';
 
-const QuestionForm = ({ onBlur }) => {
+const QuestionForm = ({ changeOrBlurAutoSave }) => {
     const { activeQuestion, watch, setValue, control, register } = useCustomFormProvider();
     const { dirtyFields, touchedFields } = useFormState({
         control
@@ -22,14 +22,14 @@ const QuestionForm = ({ onBlur }) => {
                     placeholder='your question here'
                     value={watch(`questions.${activeQuestion}.text`)}
                     {...register(`questions.${activeQuestion}.text`)}
-                    onBlur={onBlur}
+                    onBlur={changeOrBlurAutoSave}
                 />
                 <TextareaAutoSize
                     className='w-full text-lg outline-none resize-none rounded-md p-2 font-space-mono bg-transparent hover:bg-white/20 border border-transparent focus:border-gray-900 transition duration-500'
                     placeholder='description (optional)'
                     value={watch(`questions.${activeQuestion}.description`)}
                     {...register(`questions.${activeQuestion}.description`)}
-                    onBlur={onBlur}
+                    onBlur={changeOrBlurAutoSave}
                 />
 
                 {dirtyFields?.questions?.[activeQuestion]?.text && <p>Text field is dirty.</p>}
--- frontend/src/pages/CreateForm/QuestionOptions.jsx
+++ frontend/src/pages/CreateForm/QuestionOptions.jsx
@@ -23,41 +23,17 @@ const QuestionOptions = () => {
   }));
 
   return (
-    <div className="dropdown dropdown-end scale-150 absolute top-10 right-12">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img id={profileIconId} alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        </div>
+    <div className='bg-white/20 p-10 rounded-3xl w-1/5 shadow-md flex flex-col items-center justify-between'>
+      <div className='w-full max-w-xs'>
+        <Select
+          label='Question Type'
+          value={options.find((option) => option.value === currentType)}
+          onChange={(value) => setValue(`questions.${activeQuestion}.type`, value)}
+          options={options}
+        />
       </div>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-        {/* <li>
-        <Link to={'/profile'} className="justify-between">
-          Profile
-          <span className="badge">New</span>
-        </Link>
-      </li> */}
-        <li>
-          <Link to={`/user/${userId}/account`} id={accountSettingsId}>
-            Settings
-          </Link>
-        </li>
-        <li>
-          <button onClick={handleLogout}>
-            Logout
-          </button>
-        </li>
-      </ul>
-    </div>
-  )
-    < div className = 'w-full max-w-xs' >
-      <Select
-        label='Question Type'
-        value={options.find((option) => option.value === currentType)}
-        onChange={(value) => setValue(`questions.${activeQuestion}.type`, value)}
-        options={options}
-      />
       <SmallButton text='save' />
-    </div >
+    </div>
   );
 };
 
