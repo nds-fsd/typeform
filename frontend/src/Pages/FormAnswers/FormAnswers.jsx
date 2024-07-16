8abbfe8132bb5d6ad4942ac080811485c509dc63
haudacia <hadassa.codes@gmail.com>
2024-07-17 01:41:23 +0200

    WIP component AnswerCards and formAnswers (results) page layout

--- frontend/src/Pages/FormAnswers/FormAnswers.jsx
+++ frontend/src/Pages/FormAnswers/FormAnswers.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { api } from '../../utils/api';
 import style from './FormAnswers.module.css';
+import UserNavbar from '../../components/UserNavbar/UserNavbar.jsx';
+import { questionTypes } from '../../constants/questionTypes.jsx';
+import AnswerCard from './AnswerCard.jsx';
 
 const FormAnswers = () => {
   const [answers, setAnswers] = useState([]);
@@ -30,19 +33,12 @@ const FormAnswers = () => {
   }, [location]);
 
   return (
-    <div className={style.screen}>
+    <div className={'flex flex-col h-screen w-screen bg-custom-gradient bg-contain overflow-scroll'}>
+      <UserNavbar />
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {answers.length > 0 ? (
         answers.map((answerSet, index) => (
-          <div key={index} className={style.container}>
-            <h1>{answerSet.form.title}</h1>
-            {answerSet.answers.map((answerSet, index) => (
-              <div key={index} className={style.cardContainer}>
-                <h2>{answerSet.question.title}</h2>
-                <div>{answerSet.answer}</div>
-              </div>
-            ))}
-          </div>
+          <AnswerCard answerSet={answerSet} index={index} />
         ))
       ) : (
         <p>No answers to display</p>
