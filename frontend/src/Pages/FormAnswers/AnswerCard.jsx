58173c891b2d427b3c4d30f1fb524031bae4392c
haudacia <hadassa.codes@gmail.com>
2024-07-17 03:22:21 +0200

    create formAnswers table by form, WIP adjust style and format datetime

--- frontend/src/Pages/FormAnswers/AnswerCard.jsx
+++ frontend/src/Pages/FormAnswers/AnswerCard.jsx
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { api } from '../../utils/api';
-import style from './FormAnswers.module.css';
-import UserNavbar from '../../components/UserNavbar/UserNavbar.jsx';
+import React from 'react';
 import { questionTypes } from '../../constants/questionTypes.jsx';
 
-const AnswerCard = ({ answerSet, index }) => {
+const AnswerCard = ({ answer }) => {
+    const questionType = questionTypes.find(
+        (questionType) => questionType.value === answer.type
+    );
+
     return (
-        <div key={index} className={style.container}>
-            <h1>{answerSet.form.title}</h1>
-            <h2>{answerSet.creationDateTime}</h2>
-            <div className={'flex'}>
-                {answerSet.answers.map((answer, index) => {
-                    const questionType = questionTypes.find(
-                        (questionType) => (questionType.value === answer.type));
-
-                    return (
-                        <div key={index} className={style.cardContainer}>
-                            {questionType && (
-                                <div>{questionType.icon}</div>
-                            )}
-                            <h2>{answer.question.title}</h2>
-
-                            <div>{answer.answer}</div>
-                        </div>
-                    );
-                })}
-            </div>
+        <div className={'min-w-[500px] border-2 border-black p-4'}>
+            {questionType && (
+                <div>{questionType.icon}</div>
+            )}
+            <p>{answer.answer}</p>
         </div>
     );
 };
