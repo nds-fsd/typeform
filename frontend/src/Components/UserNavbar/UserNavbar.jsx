0cbfaecbec890b8eab72ad3686ff1595b91666fc
joseluishver <joseluis.hver@gmail.com>
2024-07-19 10:08:56 +0200

    last navbar changes

--- frontend/src/Components/UserNavbar/UserNavbar.jsx
+++ frontend/src/Components/UserNavbar/UserNavbar.jsx
@@ -25,7 +25,7 @@ const UserNavbar = () => {
       <UsernamesWorkspace />
       {/* <MyWorkspaceLink /> */}
       {/* <UserGreeting /> */}
-      <ShareButton formId={id} />
+      <ShareButton formId={id} publishBtnClass={"w-60 h-14 text-gray-900 shadow-md bg-azure hover:bg-white hover:shadow-none rounded-4xl transition-all duration-300 font-space-mono"} />
       <ProfileIcon accountSettingsId='accountSettings' profileIconId='profileIcon' />
       <button
         onClick={() => {
